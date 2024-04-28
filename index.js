51838bf7213fa4a17e83efe05df7ef70e6200c1c
noman5945 <abdullahnoman.5945@gmail.com>
2024-04-28 21:13:41 +0600

    add new resturent all its info to database

--- index.js
+++ index.js
@@ -30,10 +30,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    /**
+     * Collections
+     */
     const restaurantCollection = client
       .db("Checkout")
       .collection("restaurants");
+    const menuCollection = client.db("Checkout").collection("foodmenu");
 
+    /**
+     * API's
+     */
     app.get("/allrestaurants", async (req, res) => {
       const query = {};
       const cursor = restaurantCollection.find(query);
@@ -78,6 +85,21 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const rest_details = await restaurantCollection.findOne(query);
       res.send(rest_details);
+    });
+
+    /**
+     * Add New Restaurant API
+     */
+    app.post("/addrestaurant", async (req, res) => {
+      const newRest = req.body;
+      const add_rest_to_db = await restaurantCollection.insertOne(newRest);
+      res.send(add_rest_to_db);
+    });
+
+    app.post("/addfoodmenu", async (req, res) => {
+      const newMenu = req.body;
+      const add_food_menu_db = await menuCollection.insertOne(newMenu);
+      res.send(add_food_menu_db);
     });
   } finally {
   }
